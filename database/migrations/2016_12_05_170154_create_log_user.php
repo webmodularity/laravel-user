5da236577dae75553bf8553fecf30eee54cc2570
Rory Hellyer <rory@webmodularity.com>
2017-02-23 11:58:08 -0800

    add social_provider_id field to log_users

--- database/migrations/2016_12_05_170154_create_log_user.php
+++ database/migrations/2016_12_05_170154_create_log_user.php
@@ -15,13 +15,16 @@ class CreateLogUser extends Migration
     {
         Schema::create('log_users', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('log_request_id');
-            $table->unsignedInteger('user_id')->nullable();
             $table->unsignedTinyInteger('user_action');
+            $table->unsignedInteger('log_request_id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedSmallInteger('social_provider_id')->nullable();
             $table->timestamp('created_at');
             $table->index('created_at');
             $table->index(['user_id', 'user_action', 'created_at']);
             $table->index(['user_id', 'created_at']);
+            $table->index(['user_id', 'social_provider_id', 'created_at']);
+            $table->index(['social_provider_id', 'created_at']);
             $table->unique(['user_id', 'log_request_id', 'user_action']);
             $table->foreign('log_request_id')->references('id')->on('log_requests')->onUpdate('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
