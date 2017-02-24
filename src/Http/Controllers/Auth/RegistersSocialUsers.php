491226f830c308a414690a5b975af64532baa4e4
Rory Hellyer <rory@webmodularity.com>
2017-02-24 14:37:39 -0800

    social providers dev

--- src/Http/Controllers/Auth/RegistersSocialUsers.php
+++ src/Http/Controllers/Auth/RegistersSocialUsers.php
@@ -12,7 +12,7 @@ use WebModularity\LaravelUser\UserSocialProfile;
 use WebModularity\LaravelProviders\SocialProvider;
 use Laravel\Socialite\Contracts\User as SocialUser;
 use Auth;
-use DebugBar;
+use Debugbar;
 
 /**
  * Class RegistersSocialUsers
@@ -27,11 +27,11 @@ trait RegistersSocialUsers
      */
     protected function registerSocialUser(Request $request)
     {
-        DebugBar::info("Registered social user!");
+        Debugbar::info("Registered social user!");
         $socialUser = isset($request->socialUser) ? $request->socialUser : null;
         $socialProvider = isset($request->socialProvider) ? $request->socialProvider : null;
-        DebugBar::info($socialUser);
-        DebugBar::info($socialProvider);
+        Debugbar::info($socialUser);
+        Debugbar::info($socialProvider);
         if (empty($socialUser) || empty($socialProvider)) {
             return null;
         }
