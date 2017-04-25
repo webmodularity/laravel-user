eb74e8d54290b757977452a3c7c1396024b1c4ab
Rory Hellyer <rory@webmodularity.com>
2017-04-25 14:23:06 -0700

    dev

---
+++ src/Validators/UserPersonUniqueValidator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace WebModularity\LaravelUser\Validators;
+
+use WebModularity\LaravelContact\Person;
+use WebModularity\LaravelUser\User;
+
+class UserPersonUniqueValidator
+{
+
+    public function validate($attribute, $value, $parameters, $validator)
+    {
+        $person = Person::where('email', $value)->first();
+        if (is_null($person)) {
+            return true;
+        }
+        $query = User::query();
+        $query->where('person_id', $person->id);
+        if (is_array($parameters) && count($parameters)) {
+            $query->where('id', '<>', array_shift($parameters));
+        }
+
+        return $query->count() == 0;
+    }
+}
