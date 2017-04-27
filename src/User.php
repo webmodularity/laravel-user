8424e95177932b845423df556a79ae7d2b297456
Rory Hellyer <rory@webmodularity.com>
2017-04-26 18:08:46 -0700

    dev

--- src/User.php
+++ src/User.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
 
 /**
  * WebModularity\LaravelUser\User
