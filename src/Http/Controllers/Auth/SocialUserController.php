d6db68411ab1ba6eba6339e79b37647d84215b82
Rory Hellyer <rory@webmodularity.com>
2017-05-04 09:55:55 -0700

    dev

--- src/Http/Controllers/Auth/SocialUserController.php
+++ src/Http/Controllers/Auth/SocialUserController.php
@@ -64,6 +64,7 @@ class SocialUserController extends Controller
         $socialUser = $socialite->driver($socialProvider->slug)->user();
         $user = User::findFromSocialUser($socialUser, $socialProvider);
         if (is_null($user) && config('wm.user.register', false)) {
+            dd(Person::where('email', $socialUser->getEmail())->has('user')->get());
             if (!is_null(Person::where('email', $socialUser->getEmail())->has('user')->get())) {
                 // If the register attempt was unsuccessful we will increment the number of attempts
                 // to login and redirect the user back to the login form. Of course, when this
